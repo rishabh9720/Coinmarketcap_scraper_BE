a66f413c6be5208cf014593ec65b99e4a0d13668
Rishabh Gupta <rishabh.gupta1@joshtechnologygroup.com>
2024-05-09 19:47:15 +0530

    Created Django App.

---
+++ coinmarketcap_scraper/schedules.py
@@ -0,0 +1,9 @@
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    "scrap_data_from_coin_market": {
+        "task": "apps.scraper.tasks.scrape_data_from_coin_market",
+        "schedule": crontab(minute="*", second="*/15"),  # Execute every 15 seconds
+        "description": "Scrape data from coin market every 15 seconds",
+    },
+}
